76bbb83a6e5dc29a3af129cca4ab1f94602abdd5
therealalexolthoff <alex@1452design.com>
2024-07-26 14:39:55 -0400

    Create single post page

--- functions.php
+++ functions.php
@@ -9,9 +9,9 @@ function university_files() {
 };
 
 function university_features() {
-    register_nav_menu( 'headerMenuLocation', "Header Menu Description" );
-    register_nav_menu( 'footerLocationOne', "Footer1" );
-    register_nav_menu( 'footerLocationTwo', "Footer2" );
+    // register_nav_menu( 'headerMenuLocation', "Header Menu Description" );
+    // register_nav_menu( 'footerLocationOne', "Footer1" );
+    // register_nav_menu( 'footerLocationTwo', "Footer2" );
     add_theme_support('title-tag');
 }
 add_action('wp_enqueue_scripts','university_files'); 
